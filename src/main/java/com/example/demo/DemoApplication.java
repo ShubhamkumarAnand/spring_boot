c4f31959e8bf6d3820e8c54b9b41ce35ba8f7be5
imskanand <the.anand3099@gmail.com>
2022-09-17 17:56:33 +0530

    Making the API V1
                ++ Added the Student class with id, name, email, dob, age
                ++ Added the Student controller
                ++ Passed the data and API V1 is working

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,9 +13,6 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "Welcome to the Spring Bootcamp!");
-	}
+
 
 }
