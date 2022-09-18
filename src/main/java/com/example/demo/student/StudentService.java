dce36d33a4793942a510b27ed19f95c04f281f86
imskanand <the.anand3099@gmail.com>
2022-09-18 11:18:49 +0530

    Implemented the PUT Method
    -- provided the studentId and modifiable value of name and email it will update the value to the provided ID
    -- First check is done on email for unique but nothing such on the name
    -- Made a new method on put and implemented it.
    
    Testing Done of PUT Method!✅

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -29,9 +31,32 @@ public class StudentService {
 
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
-        if(!exists){
-            throw new IllegalStateException("Student with the "+ studentId + " does not exists");
+        if (!exists) {
+            throw new IllegalStateException("Student with the " + studentId + " does not exists");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId).
+                orElseThrow(() -> new IllegalStateException(
+                        "Student with the " + studentId + " does not exists"
+                ));
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email Already Taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
